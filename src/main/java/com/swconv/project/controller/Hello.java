a680b257261169fe9450583ffefeb91541623365
kapr <kyowon1108@gmail.com>
2023-04-28 03:31:06 +0900

    Initial commit

---
+++ src/main/java/com/swconv/project/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.swconv.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class Hello {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
