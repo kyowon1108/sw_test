949b0c537b32ecc3ff3273157ea54eeb2e54e3c0
kapr <kyowon1108@gmail.com>
2023-04-28 15:34:34 +0900

    mysql test

--- src/main/java/com/swconv/project/controller/MysqlTest.java
+++ src/main/java/com/swconv/project/controller/MysqlTest.java
@@ -3,19 +3,30 @@ package com.swconv.project.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.sql.*;
 
-
 class MysqlConnect {
-    @Value("${spring.datasource.driver-class-namel}")
-    static String JDBC_DRIVER;
+    /*
+    @Value("${spring.datasource.driver-class-name}")
+    public String JDBC_DRIVER;
     @Value("${spring.datasource.url}")
-    static String DB_URL;
-    @Value("${spring.datasource.usernam}")
-    static String USER;
+    public String DB_URL;
+    @Value("${spring.datasource.username}")
+    public String USER;
     @Value("${spring.datasource.password}")
+    public String PASS;
+    */
+    static String DB_URL;
+    static String USER;
     static String PASS;
+    static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 
+    public MysqlConnect (String DB_URL, String USER, String PASS) {
+        this.DB_URL = DB_URL;
+        this.USER = USER;
+        this.PASS = PASS;
+    }
     static String connect() {
         Connection conn = null;
         Statement stmt = null;
@@ -67,8 +78,15 @@ class MysqlConnect {
 @RestController
 public class MysqlTest {
 
+    @Value("${spring.datasource.url}")
+    private String URL;
+    @Value("${spring.datasource.username}")
+    private String USER;
+    @Value("${spring.datasource.password}")
+    private String PASS;
     @GetMapping(value = "/mysql")
     public String test_mysql(){
-        return MysqlConnect.connect();
+        MysqlConnect a = new MysqlConnect(URL, USER, PASS);
+        return a.connect();
     }
 }
